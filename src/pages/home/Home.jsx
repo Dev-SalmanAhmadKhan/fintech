2a72066c8d9874e1749e694f41fa8e9d360924af
Salman Ahmad Khan <salmanahmadkhan001@gmail.com>
2023-07-26 18:52:26 +0500

    about us section along with achievements cards

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import Header from "../../components/common/header/Header";
 import About from "../../components/pages/Home/Aboutus/About";
 import Hero from "../../components/pages/Home/Hero/Hero";
 import Services from "../../components/pages/Home/Services/Services";
+import Achievements from "../../components/pages/Home/achievements/Achievements";
 
 import "./home.scss";
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
       </div>
       <Services />
       <About />
+      <Achievements/>
     </>
   );
 };
