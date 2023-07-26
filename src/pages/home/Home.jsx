62124661257370b8adf2d999fc6e1c9db68ff675
Salman Ahmad Khan <salmanahmadkhan001@gmail.com>
2023-07-26 19:29:45 +0500

    achievements section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,5 @@
 import About from "../../components/pages/Home/Aboutus/About";
 import Achievements from "../../components/pages/Home/achievements/Achievements";
-
 import "./home.scss";
 const Home = () => {
   return (
