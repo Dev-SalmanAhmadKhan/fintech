6e711b5916c23a4b7f424e21d283fc1143d9fe98
Salman Ahmad Khan <salmanahmadkhan001@gmail.com>
2023-07-27 02:19:03 +0500

    Achievements section along with about us and header

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import Header from "../../components/common/header/Header";
 import About from "../../components/pages/Home/Aboutus/About";
 import Hero from "../../components/pages/Home/Hero/Hero";
 import Services from "../../components/pages/Home/Services/Services";
+import Achievements from "../../components/pages/Home/Achievements/Achievements";
 import "./home.scss";
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       </div>
       <Services />
       <About />
+      <Achievements/>
     </>
   );
 };
