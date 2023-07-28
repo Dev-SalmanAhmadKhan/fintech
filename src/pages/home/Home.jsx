5decba4b27cc8bf35aa9cc5ab84dfafc46e2430f
Salman Ahmad Khan <salmanahmadkhan001@gmail.com>
2023-07-28 15:47:17 +0500

    Main merging with header

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,7 @@
 import About from "../../components/pages/Home/Aboutus/About";
+import Hero from "../../components/pages/Home/Hero/Hero";
+import Services from "../../components/pages/Home/Services/Services";
+import Achievements from "../../components/pages/Home/Achievements/Achievements";
 import Achievements from "../../components/pages/Home/Achievements/Achievements";
 import "./home.scss";
 const Home = () => {
