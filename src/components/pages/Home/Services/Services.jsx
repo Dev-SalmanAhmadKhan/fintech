98ed12ab504c6eb456a4a3347efbed140b0b5bc7
Salman Ahmad Khan <salmanahmadkhan001@gmail.com>
2023-07-28 16:03:35 +0500

    main conflicts resolving

---
+++ src/components/pages/Home/Services/Services.jsx
@@ -0,0 +1,30 @@
+import { motion } from "framer-motion";
+import ServicesCard from "./ServicesCard";
+import "./services.scss";
+import SectionHeading from "../../../layouts/SectionHeader/SectionHeader";
+import { Button } from "antd";
+const Services = () => {
+  return (
+    <>
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 100 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate="visible"
+        transition={{ delay: 0.3, duration: 2 }}
+      >
+        <SectionHeading class={"title"}>Services We Provide</SectionHeading>
+        <div className="services-section">
+          <div className="services-card-holder">
+            <ServicesCard />
+          </div>
+          <Button className="services-btn">Learn more</Button>
+        </div>
+      </motion.div>
+    </>
+  );
+};
+
+export default Services;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,17 @@
+import Header from "../../components/common/header/Header";
 import About from "../../components/pages/Home/Aboutus/About";
 import Hero from "../../components/pages/Home/Hero/Hero";
 import Services from "../../components/pages/Home/Services/Services";
-import Achievements from "../../components/pages/Home/Achievements/Achievements";
 import Achievements from "../../components/pages/Home/Achievements/Achievements";
 import "./home.scss";
 const Home = () => {
   return (
     <>
+    <div className="herosection">
+    <Header/>
+    <Hero/>
+    </div>
+    <Services/>
       <About />
       <Achievements/>
     </>
